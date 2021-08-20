bf1cc83c78fd53ce71e77c09bbfeccd748530acb
sclipse <karlo.versic@gmail.com>
2021-08-20 13:14:37 +0200

    Added search support for 2 visible months (current and next one), added console logs to easily follow workflow, added "--headless" argument to browser, fixed empty filter printing empty table (empty filter is now being ignored), fixed wrong table dimensions when no result match filter, renamed "apartments_list.txt" to "filter.txt" (more intuitive).

--- main.py
+++ main.py
@@ -2,8 +2,89 @@ import codecs
 import logging
 
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 logging.basicConfig()
+
+
+# Date entry
+def date_input(months):
+    date = input("Please enter desired date (DD.MM): ").split('.')
+
+    # ValiDATE
+    if int(date[1]) > 12 or int(date[1]) < 1:
+        print("Entered month invalid.")
+        date_input(months)
+        return date
+    elif int(date[0]) > months[int(date[1]) - 1] or int(date[0]) < 1:
+        print("Entered date invalid.")
+        date_input(months)
+        return date
+    return date
+
+
+# Convert Selenium webelement to string
+def webelement_to_text(item_list):
+    list_text = []
+    for item in item_list:
+        list_text.append(item.text)
+    return list_text
+
+
+# Select desired date on calendar
+def get_date(driver):
+    months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+    # Get "today", today's year, and today's month
+    today = driver.find_element_by_class_name('bui-calendar__date--today').get_attribute("data-date").split("-")
+    today_year = int(today[0])
+    today_month = int(today[1])
+
+    # Check for leap year
+    if today_year % 4 == 0:
+        months[1] = 29
+
+    date = input("Please enter desired date (DD.MM): ").split('.')
+
+    # ValiDATE
+    success = 0
+    while success != 1:
+        if len(date) == 1:
+            print('Entered day and month invalid or missing dot.')
+            date = input("Please enter desired date (DD.MM): ").split('.')
+            continue
+        elif date[1] == '' or int(date[1]) > 12 or int(date[1]) < 1:
+            print("Entered month invalid.")
+            date = input("Please enter desired date (DD.MM): ").split('.')
+            continue
+        elif date[0] == '' or int(date[0]) > months[int(date[1]) - 1] or int(date[0]) < 1:
+            print("Entered day invalid.")
+            date = input("Please enter desired date (DD.MM): ").split('.')
+            continue
+        else:
+            success = 1
+
+    # Get all calendar entries that are valid
+    cal_entries = webelement_to_text(driver.find_elements_by_class_name('bui-calendar__date'))
+
+    # Check on which calendar pane (1st or 2nd) is desired month
+    additional_days = -1
+    if int(date[1]) != today_month:
+        additional_days = 41
+
+    print("Locating desired date's webelement...")
+    # Find desired date on calendar
+    blanks = -1
+    for entry in cal_entries:
+        if entry == '' or blanks < additional_days:
+            blanks += 1
+            continue
+        elif entry == date[0]:
+            # driver.find_elements_by_class_name('bui-calendar__date')[int(date[0])].click()
+            driver.find_elements_by_class_name('bui-calendar__date')[int(date[0]) + blanks].click()
+            break
+
+    return
 
 
 # Set up search query
@@ -12,28 +93,11 @@ def search(driver):
     driver.find_element_by_name("ss").send_keys("Rijeka")
     # Dates table
     driver.find_element_by_xpath('//*[@id="frm"]/div[1]/div[2]/div[1]/div[2]/div/div/div/div/span').click()
-    # Today
-    driver.find_element_by_class_name('bui-calendar__date--today').click()
+    # Date
+    get_date(driver)
     # Search button
-    driver.find_element_by_xpath('//*[@id="frm"]/div[1]/div[4]/div[2]/button').click()
-
-
-# Retrieve names from current page
-def get_names(driver):
-    aparts_name = driver.find_elements_by_class_name("sr-hotel__name")
-    aparts_name_text = []
-    for name in aparts_name:
-        aparts_name_text.append(name.text)
-    return aparts_name_text
-
-
-# Retrieve prices from current page
-def get_prices(driver):
-    aparts_price = driver.find_elements_by_class_name("bui-price-display__value")
-    aparts_price_text = []
-    for price in aparts_price:
-        aparts_price_text.append(price.text)
-    return aparts_price_text
+    print("Search submitted.")
+    driver.find_element_by_class_name('-submit-button').click()
 
 
 # Convert 2D list to 1D list
@@ -44,6 +108,7 @@ def parse(list_unparsed):
         for item in items:
             list_parsed.append(item)
 
+            # Get max item length to know prices.txt table dimensions
             if len(item) > max_len:
                 max_len = len(item)
     return list_parsed, max_len
@@ -57,29 +122,42 @@ def adjust_length(name, max_len):
 
 
 # Print results in table in prices.txt file
-def print_result(names, prices, max_name_len, max_price_len):
+def fprint_result(names, prices, max_name_len, max_price_len):
     file = codecs.open("prices.txt", 'w+', 'utf-8')
 
-    file.write("#" * (max_name_len + max_price_len + 15) + "\n")
+    num_of_hashs = 15
+    if not names:
+        num_of_hashs = 30
+
+    file.write("#" * (max_name_len + max_price_len + num_of_hashs) + "\r\n")
     file.write("#   " + adjust_length("Apartmani", max_name_len) + "   #   " + adjust_length("Cijene",
-                                                                                             max_price_len) + "   #" + "\n")
-    file.write("#" * (max_name_len + max_price_len + 15) + "\n")
-    for name, price in zip(names, prices):
-        file.write("#   " + adjust_length(name, max_name_len) + "   #   " + adjust_length(price,
-                                                                                          max_price_len) + "   #" + "\n")
-    file.write("#" * (max_name_len + max_price_len + 15))
+                                                                                             max_price_len) + "   #\r\n")
+    file.write("#" * (max_name_len + max_price_len + num_of_hashs) + "\r\n")
+    if names:
+        for name, price in zip(names, prices):
+            file.write("#   " + adjust_length(name, max_name_len) + "   #   " + adjust_length(price,
+                                                                                              max_price_len) + "   #\r\n")
+    else:
+        file.write("#     No apartments found    #\r\n")
+    file.write("#" * (max_name_len + max_price_len + num_of_hashs))
 
     file.close()
+    return
 
 
 def filter_result(names, prices):
     # noinspection PyBroadException
     try:
-        with open('apartments_list.txt', 'r') as file:
+        with open('filter.txt', 'r') as file:
             filter_list = set(file.read().splitlines())
     except Exception:
         return names, prices
 
+    if not filter_list:
+        print("No filter set in filter.txt., ignoring filter.txt.")
+        return names, prices
+
+    print("Filter applied.")
     names_filtered = []
     prices_filtered = []
     for name, price in zip(names, prices):
@@ -91,19 +169,30 @@ def filter_result(names, prices):
 
 
 def main():
-    # Open booking.com in chromium
-    driver = webdriver.Chrome()
+    print("Opening Chromium...")
+    # Open booking.com in headless Chromium
+    options = Options()
+    options.add_argument('headless')
+    options.add_argument('disable-gpu')
+    options.add_argument("window-size=1920,1080")
+    # Bypass "headless" mode block
+    user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
+    options.add_argument(f'user-agent={user_agent}')
+
+    driver = webdriver.Chrome(options=options)
     driver.get("https://www.booking.com")
 
-    # Search for Rijeka and today's date
+    # Search for Rijeka and desired date
+    print("Setting up search...")
     search(driver)
+
+    print("Getting apartments names and prices...")
 
     names_unparsed = []
     prices_unparsed = []
-
     # Get unparsed names and prices from first page
-    names_unparsed.append(get_names(driver))
-    prices_unparsed.append(get_prices(driver))
+    names_unparsed.append(webelement_to_text(driver.find_elements_by_class_name("sr-hotel__name")))
+    prices_unparsed.append(webelement_to_text(driver.find_elements_by_class_name("bui-price-display__value")))
 
     # Find how many pages exists
     res_found = 0
@@ -111,7 +200,6 @@ def main():
     try:
         # (Rijeka: 14 properties found).text.split(' ')[1] = 14
         res_found = int(driver.find_element_by_class_name("sr_header").text.split(' ')[1])
-
     except Exception:
         pass
 
@@ -120,6 +208,7 @@ def main():
     if res_found == '':
         pass
     elif res_found > res_per_page:
+        print("Going through pages...")
         # First page url
         url = driver.current_url
 
@@ -132,8 +221,8 @@ def main():
             # Go to next page using offset
             driver.get(url + '&offset=' + str(offset))
 
-            names_unparsed.append(get_names(driver))
-            prices_unparsed.append(get_prices(driver))
+            names_unparsed.append(webelement_to_text(driver.find_elements_by_class_name("sr-hotel__name")))
+            prices_unparsed.append(webelement_to_text(driver.find_elements_by_class_name("bui-price-display__value")))
 
             offset += 25
 
@@ -147,13 +236,29 @@ def main():
     prices_parsed = prices_parsed[0]
 
     # Filter only wanted apartments
+    print("Filtering apartments...")
     result = filter_result(names_parsed, prices_parsed)
+
+    # If filter applied, adjust max item length for prices.txt table dimensions
+    if len(names_parsed) > len(result[0]):
+        max_name_len = 0
+        max_price_len = 0
+        for name, price in zip(result[0], result[1]):
+            if len(name) > max_name_len:
+                max_name_len = len(name)
+            if len(price) > max_price_len:
+                max_price_len = len(price)
+
+    # Separate names from prices
     names_parsed = result[0]
     prices_parsed = result[1]
 
     # Print to prices.txt
-    print_result(names_parsed, prices_parsed, max_name_len, max_price_len)
+    print("Printing results to prices.txt...")
+    fprint_result(names_parsed, prices_parsed, max_name_len, max_price_len)
+    print("Results printed.")
 
+    print("Closing Chromium...")
     driver.quit()
 
 

36de17f0628b7d036b9456a599c400bf3daada95
Sclipse <59739179+Sclipse@users.noreply.github.com>
2021-08-21 14:30:19 +0200

    Optimized imports.

--- main.py
+++ main.py
@@ -3,9 +3,6 @@ import logging
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 logging.basicConfig()
 
